c6f8ede8eb924aa5d55dac27538c7db5169acaaa
Krzysztof Branecki <krzysiek091290@gmail.com>
2019-10-22 20:33:42 +0200

    Add component and style from UX Designer

---
+++ client/src/components/common/Logo/Logo.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import './Logo.scss';
+
+const Logo = ({ links, location }) => <h1 className="logo">Blog.app</h1>;
+
+export default Logo;
---
+++ client/src/components/common/TextField/TextField.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import './TextField.scss';
+
+const TextField = ({ value, label, onChange, ...otherProps }) => (
+  <label className="text-field" htmlFor="text-field">
+    <span className={`text-field__label ${!value.length > 0 ? 'text-field__label--big' : ''}`}>
+      {label}
+    </span>
+    <input
+      {...otherProps}
+      value={value}
+      name="text-field"
+      onChange={onChange}
+      className="text-field__input"
+    />
+  </label>
+);
+
+TextField.propTypes = {
+  value: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+};
+
+export default TextField;
