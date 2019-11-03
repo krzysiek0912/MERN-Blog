8d251e713d7e731d1e885d950410a5a6d251bb43
Krzysztof Branecki <krzysiek091290@gmail.com>
2019-11-03 20:02:27 +0100

    Add content to Home Page and Contact Page and add sanitize in server

--- client/src/components/pages/Contact/ContactPages.js
+++ client/src/components/pages/Contact/ContactPages.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import PageTitle from '../../common/PageTitle/PageTitle';
 
 const ContactPage = () => (
   <div>
-    <h1>Contact</h1>
+    <PageTitle>Contact</PageTitle>
+    <p>Your Favourite Blooger Ltd.</p>
+    <p>9432 Division Street Stone </p>
+    <p>Mountain, GA 30083 </p>
+    <p> </p>
+    <p>Mobile Number: 207-914-3411</p>
   </div>
 );
 
--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
 const config = require('./config');
 const loadTestData = require('./testData');
 
@@ -14,6 +15,7 @@ app.use(helmet());
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(mongoSanitize());
 app.use('/api', postRoutes);
 
 // connects our back end code with the database
