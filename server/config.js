24c79a7817b629ba58d7a6a7446e17c7c7067d14
Krzysztof Branecki <krzysiek091290@gmail.com>
2019-11-07 11:06:10 +0100

    Add varible  process.env.DB to config.js in server

--- server/config.js
+++ server/config.js
@@ -1,5 +1,4 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
-  DB:
-    'mongodb+srv://mongomern:3sZxO2gEgNmdH6y8@mern-7mxf5.mongodb.net/test?retryWrites=true&w=majority',
+  DB: process.env.DB,
 };
